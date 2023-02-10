739f34eeecf2ce72d84ad6cefe7fb843e78891a3
Sam Guard <sg2268@bath.ac.uk>
2023-02-10 14:41:55 +0000

    Printing

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
                 pixels = data["pixels"]
                 timestamps = data["timestamps"]
                 #timestamps = torch.zeros(size=(pixels.shape[0],1), device="cuda:0") + data["timestamps"]
-
+                print(rays.shape)
                 # update occupancy grid
                 occupancy_grid.every_n_step(
                     step=step,
