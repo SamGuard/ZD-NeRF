cf52fc7ac945d20b8895b260d97140f80499e520
Sam Guard <sg2268@bath.ac.uk>
2023-02-13 20:32:27 +0000

    decreased grid size

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     # setup the dataset
     data_root_fp = "/home/ruilongli/data/dnerf/"
     target_sample_batch_size = 1 << 16
-    grid_resolution = 128
+    grid_resolution = 64
 
     train_dataset = SubjectLoader(
         subject_id=args.scene,
