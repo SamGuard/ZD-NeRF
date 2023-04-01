07f38b2e30cccfcd7ddd179aa40b510a3b825900
Sam Guard <sg2268@bath.ac.uk>
2023-04-01 23:19:29 +0100

    changed renderer

--- main.py
+++ main.py
@@ -368,7 +368,7 @@ if __name__ == "__main__":
                     ),
                 )
 
-            for t in map(lambda x: x / num_time, range(num_time)):
+            for t in map(lambda x: x / (num_time-1), range(num_time)):
                 for i in [11]:  # range(len(test_dataset)):
                     data = test_dataset[i]
                     render_bkgd = data["color_bkgd"]
