f440d0e043ecc4604697df8ff115eea7dbfd9537
Sam Guard <sg2268@bath.ac.uk>
2023-04-04 16:58:01 +0100

    alpha change

--- main.py
+++ main.py
@@ -216,7 +216,8 @@ if __name__ == "__main__":
                     render_step_size=render_step_size,
                     render_bkgd=render_bkgd,
                     cone_angle=args.cone_angle,
-                    alpha_thre=0.01 if step > 1000 else 0.00,
+                    # Temporary change as may be causing issues, to revert change first value to 0.01
+                    alpha_thre=0.00 if step > 1000 else 0.00,
                     # dnerf options
                     timestamps=timestamps,
                 )
