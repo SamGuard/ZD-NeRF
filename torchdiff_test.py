a948e6e25b858b7921454b070445ebc8cdeaa0e7
Sam Guard <sg2268@bath.ac.uk>
2023-04-13 21:48:16 +0100

    made test more realistic

--- torchdiff_test.py
+++ torchdiff_test.py
@@ -5,18 +5,17 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
+steps = 50
+num_data = 2**15
+x = torch.rand(size=(num_data, 3)).to(DEVICE)
+y = torch.rand(size=(steps - 1, num_data, 3)).to(DEVICE)
+t = torch.linspace(0, 1, steps).to(DEVICE)
+odefunc = NeuralField(4, 3, 32, 8).to(DEVICE)
+
 
 def test():
-    steps = 50
-    odefunc = NeuralField(4, 3, 32, 8).to(DEVICE)
-    num_data = 2**15
-    x = torch.rand(size=(num_data, 3)).to(DEVICE)
-    y = torch.rand(size=(steps - 1, num_data, 3)).to(DEVICE)
-    t = torch.linspace(0, 1, steps).to(DEVICE)
-
     optim = torch.optim.Adam(odefunc.parameters())
     for i in range(2):
-        
         pred = odeint(
             func=odefunc,
             y0=x,
@@ -30,8 +29,15 @@ def test():
         optim.step()
         print(f"Step {i}, loss {loss}")
 
-with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+
+with profile(
+    activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True
+) as prof:
     with record_function("model_inference"):
-      test()
-  
-open("profiler.out", "w").write(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=100))
+        test()
+
+open("profiler.out", "w").write(
+    prof.key_averages(group_by_input_shape=True).table(
+        sort_by="cpu_time_total", row_limit=100
+    )
+)
