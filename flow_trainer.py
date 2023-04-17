91e77f9d4a1eace016c22c8bd4b73cb56aa1b25f
Sam Guard <sg2268@bath.ac.uk>
2023-04-17 20:23:47 +0100

    changed training to a shooting like method

--- flow_trainer.py
+++ flow_trainer.py
@@ -4,73 +4,136 @@ from torch import nn
 
 import numpy as np
 
-from torchdiffeq import odeint_adjoint as odeint
-
+from torchdiffeq import odeint as odeint
+from libs.torchdyn.torchdyn.numerics import odeint_mshooting
+from libs.torchdyn.torchdyn.core.neuralde import NeuralODE
 
 def train_flow_field(
-    odefunc: nn.Module, timestamps: torch.Tensor, points_base: torch.Tensor, epochs=50, alpha=0.05
+    odefunc: nn.Module,
+    timestamps_base: torch.Tensor,
+    points_base: torch.Tensor,
+    epochs=50,
+    steps_ahead=2,
 ):
     """points_base.requires_grad_()
     timestamps.requires_grad_()"""
 
     optimizer = torch.optim.Adam(odefunc.parameters(), 5e-3)
-    sched = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=1-1e-2, verbose=True)
+    sched = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer,
+        milestones=[
+            epochs // 2,
+            epochs * 3 // 4,
+            epochs * 5 // 6,
+            epochs * 9 // 10,
+        ],
+        gamma=0.1,
+    )
 
     step = 0
     batch_size = 1
     loss = 0
     go = True
     while go:
-        points = points_base + torch.rand_like(points_base, device=points_base.device) * alpha
-        pred = odeint(odefunc, points[0], timestamps, rtol=1e-4, atol=1e-5)[1:]
-        loss += F.smooth_l1_loss(pred, points[1:])
-        if(step % batch_size == 0):
+        # Add random noise
+        # points = points_base + torch.rand_like(points_base, device=points_base.device) * alpha
+        is_reversed = 1 if torch.rand(size=(1,)) > 0.5 else -1
+        start_t = int(torch.rand(size=(1,)) * (len(timestamps_base) - steps_ahead))
+        start_t += steps_ahead if is_reversed == -1 else 0
+        start_t
+        end_t = start_t + is_reversed * steps_ahead
+
+        points = (
+            torch.flip(points_base[end_t : start_t + 1], dims=(0,))
+            if is_reversed == -1
+            else points_base[start_t : end_t + 1]
+        )
+
+        timestamps = (
+            torch.flip(timestamps_base[end_t : start_t + 1], dims=(0,))
+            if is_reversed == -1
+            else timestamps_base[start_t : end_t + 1]
+        )
+
+        pred = odeint(
+            odefunc,
+            points[0],
+            timestamps,
+            rtol=1e-7,
+            atol=1e-9,
+        )[1:]
+        loss += F.mse_loss(pred, points[1:])
+        if step % batch_size == 0:
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             sched.step()
             print(f"flow field: step {step} loss {loss}")
             loss = 0
-        if(step >= epochs):
+        if step >= epochs:
             go = False
             break
 
         # grad_scaler.scale(loss).backward()
         step += 1
 
+
 def train_flow_field_old(
-    warp: nn.Module, timestamps: torch.Tensor, points: torch.Tensor, epochs=50
+    odefunc: nn.Module,
+    timestamps: torch.Tensor,
+    points_base: torch.Tensor,
+    epochs=50,
+    alpha=0.05,
+    ms_shooting=True,
 ):
-    points.requires_grad_()
-    timestamps.requires_grad_()
+    """points_base.requires_grad_()
+    timestamps.requires_grad_()"""
 
-    optimizer = torch.optim.Adam(warp.parameters(), 5e-2)
-    sched = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=1-5e-4, verbose=True)
+    optimizer = torch.optim.Adam(odefunc.parameters(), 5e-3)
+    sched = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer,
+        milestones=[
+            epochs // 2,
+            epochs * 3 // 4,
+            epochs * 5 // 6,
+            epochs * 9 // 10,
+        ],
+        gamma=0.33,
+    )
 
+    loss_scaler = 0.95 ** torch.linspace(0, len(timestamps) - 2, len(timestamps) - 1)
+    loss_scaler = loss_scaler.unsqueeze(1).unsqueeze(1)
     step = 0
     batch_size = 1
-    dist_loss = 0
-    vel_loss = 0
+    loss = 0
     go = True
     while go:
-        for t1, t2, p1, p2 in zip(
-            timestamps[:-1], timestamps[1:], points[:-1], points[1:]
-        ):
-            pred_p2 = warp(t1, t2, p1)
-            dist_loss += F.smooth_l1_loss(pred_p2, p2, beta=0.01)
-            vel_loss += F.l1_loss(warp.odefunc(t1, p1), torch.zeros_like(p1))
-            if(step % batch_size == 0):
-                optimizer.zero_grad()
-                total_loss = dist_loss + 1e-2 * vel_loss 
-                total_loss.backward()
-                optimizer.step()
-                sched.step()
-                print(f"flow field: step {step} loss {total_loss} {dist_loss} {vel_loss}")
-                dist_loss = 0
-                vel_loss = 0    
-            if(step >= epochs):
-                go = False
-                break
+        # Add random noise
+        # points = points_base + torch.rand_like(points_base, device=points_base.device) * alpha
+        points = points_base
+        if ms_shooting:
+            pred = odeint_mshooting(
+                odefunc,
+                points[0],
+                timestamps,
+                solver="mszero",
+                B0=points.clone(),
+                fine_steps=len(points),
+            )[1][1:]
+        else:
+            pred = odeint(odefunc, points[0], timestamps, rtol=1e-7, atol=1e-9)[1:]
+        loss += torch.mean(((pred - points[1:]) * loss_scaler) ** 2)
+        if step % batch_size == 0:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            sched.step()
+            print(f"flow field: step {step} loss {loss}")
+            loss = 0
+        if step >= epochs:
+            go = False
+            break
 
-            # grad_scaler.scale(loss).backward()
-            step += 1
+        # grad_scaler.scale(loss).backward()
+        step += 1
+
---
+++ flow_trainer_test.py
@@ -0,0 +1,200 @@
+import torch
+from datasets.dnerf_synthetic import load_json_file, load_verts
+from mlp import (
+    ZD_NeRFRadianceField,
+    DivergenceFreeNeuralField,
+    ODEBlock_Forward,
+    NeuralField,
+    CurlField,
+)
+from torchdiffeq import odeint_adjoint
+import matplotlib.pyplot as plt
+from flow_trainer import *
+
+from libs.nerflow.run_nerf_helpers import NeRF
+
+import time
+
+
+class DivergenceFreeNeuralFieldAnnealing(nn.Module):
+    def __init__(self, spatial_dims=3, other_inputs=1, width=32, depth=8):
+        super().__init__()
+
+        self.spatial_dims = spatial_dims
+        self.other_inputs = other_inputs
+
+        # One full network for each output dim
+        networks = nn.ModuleList()
+        for i in range(spatial_dims):
+            layers = nn.Sequential(nn.Linear(spatial_dims + other_inputs, width))
+            layers.append(nn.Tanh())
+            for i in range(depth - 2):
+                layers.append(nn.Linear(width, width))
+                layers.append(nn.Tanh())
+            layers.append(nn.Linear(width, 1))
+            networks.append(layers)
+
+        self.networks = networks
+
+        self.trace_params = torch.nn.ParameterList()
+        for i in range(spatial_dims - 1):
+            self.trace_params.append(torch.randn((1,)))
+
+    def forward(self, t: torch.Tensor, x: torch.Tensor):
+        t = torch.zeros((x.shape[0], 1), device=t.device) + t
+        output = torch.zeros(x.shape[0], self.spatial_dims, device=x.device)
+        trace_param_residual = torch.sum(
+            torch.tensor(self.trace_params, device=x.device)
+        )
+        x = torch.cat((x, t), dim=1)
+        for i in range(self.spatial_dims):
+            mask = torch.zeros(size=(x.shape[0], self.spatial_dims + self.other_inputs))
+
+            output[:, i] = self.networks[i](x).squeeze()
+            if i < self.spatial_dims - 1:
+                output[:, i] += self.trace_params[i] * x[:, i]
+            else:
+                # The last dimension is the negative trace of the
+                # previous matrix if it was one dimension smaller
+                # The ensures the trace == 0
+                output[:, i] += -trace_param_residual * x[:, i]
+        return output
+
+
+def train_flow_field_mshooting(
+    warp: nn.Module, timestamps: torch.Tensor, points: torch.Tensor, epochs=50
+):
+    points.requires_grad_()
+    timestamps.requires_grad_()
+
+    optimizer = torch.optim.Adam(warp.parameters(), 0.01)
+
+    step = 0
+    p_target = points[0].clone()
+    while step < epochs:
+        pred_points = warp(
+            timestamps,
+            points[-1].clone(),
+        )
+
+        print(pred_points)
+        print(pred_points.shape)
+
+        loss = F.l1_loss(p_target, 0)
+        optimizer.zero_grad()
+        # grad_scaler.scale(loss).backward()
+        loss.backward()
+        optimizer.step()
+        print(f"flow field: step {step} loss {loss}")
+        step += 1
+
+
+def plot_points(points, ax, pause=None):
+    ax.axes.set_xlim3d(left=-3, right=3)
+    ax.axes.set_ylim3d(bottom=-3, top=3)
+    ax.axes.set_zlim3d(bottom=-3, top=3)
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], alpha=0.1)
+    if pause == None:
+        plt.show()
+    else:
+        plt.pause(pause)
+
+
+def test(func: DivergenceFreeNeuralField, timestamps, points):
+    timestamps = torch.linspace(0, 1, 11)
+    out = odeint_adjoint(func, points[0], timestamps).detach().numpy()
+    for i, t in enumerate(timestamps[:]):
+        fig = plt.figure(0)
+        ax = fig.add_subplot(projection="3d")
+        plot_points(out[i], ax, pause=None)
+
+    return
+
+    fig = plt.figure(0)
+    ax = fig.add_subplot(projection="3d")
+    p_in = points[0]
+    plot_points(p_in.detach().numpy(), ax, pause=0.5)
+    gap = 1 / len(timestamps)
+
+    for i, t in enumerate(timestamps[:-1]):  # torch.linspace(0, 1, 22)
+        p_out = warp(
+            t,
+            t + gap,
+            p_in.clone(),
+        )
+        print(t, t + gap)
+
+        # diff = (p_out - p_in).detach().numpy()
+        # p = p_in.detach().numpy()
+        """ax = plt.figure().add_subplot(projection="3d")
+        ax.axes.set_xlim3d(left=-5, right=5)
+        ax.axes.set_ylim3d(bottom=-5, top=5)
+        ax.axes.set_zlim3d(bottom=-5, top=5)    
+        ax.quiver(
+            p[:, 0],
+            p[:, 1],
+            p[:, 2],
+            diff[:, 0],
+            diff[:, 1],
+            diff[:, 2],
+            normalize=True
+        )
+        plt.show()"""
+
+        plot_points(p_out.detach().numpy(), ax, pause=0.5)
+        p_in = p_out  # points[i + 1]
+
+
+def eval_flow(flow: callable, res=8):
+    scene_aabb = torch.tensor([-5, -5, -5, 5, 5, 5], dtype=torch.float32)
+    aabb_size = float(torch.abs((scene_aabb[3:] - scene_aabb[:3]))[0])
+    lin = torch.linspace(0, aabb_size, res, dtype=torch.float32)
+    x, y, z = torch.meshgrid((lin, lin, lin), indexing="xy")
+    sample_points = torch.stack((x, y, z), dim=3).reshape(-1, 3)
+    p = sample_points.detach().numpy()
+    for t in torch.linspace(0, 1, 10):
+        vals = flow(t.unsqueeze(0), sample_points).detach().numpy()
+        print(vals)
+        ax = plt.figure().add_subplot(projection="3d")
+        ax.quiver(
+            p[:, 0], p[:, 1], p[:, 2], vals[:, 0], vals[:, 1], vals[:, 2], length=1.0
+        )
+        plt.show()
+
+
+timestamps_base, points = load_verts(load_json_file(".", "train"))
+
+indexs = torch.linspace(0, len(timestamps_base) - 1, 25, dtype=torch.long)
+timestamps_base = timestamps_base[indexs]
+points = points[indexs]
+
+radiance_field = ZD_NeRFRadianceField()
+train = True
+#radiance_field.load_state_dict(torch.load("models/bouncy10k.pt", map_location="cpu"))
+radiance_field.train(train)
+warp = ODEBlock_Forward(DivergenceFreeNeuralField(3, 1, 16, 8, torch.nn.ReLU)) #radiance_field.warp
+# warp = ODEBlock_Forward(CurlField(NeuralField(4, 3, 16, 3)))
+# warp = ODEBlock_Forward(NeuralField(4, 3, 32, 8))
+# warp = ODEBlock_MS(DivergenceFreeNeuralField(width=16, depth=5))
+#warp = ODEBlock_Forward(NeRF(sin_init=True).velocity_module)
+
+fig = plt.figure(0)
+ax = fig.add_subplot(projection="3d")
+#points += torch.rand_like(points, device=points.device) * 0.1
+for p in points:
+    plot_points(p.detach().numpy(), ax, pause=.1)
+plt.show()
+
+if train:
+    t_start = time.time()
+    train_flow_field(
+        warp.odefunc,
+        timestamps_base,
+        points,
+        epochs=1000, steps_ahead=4
+    )
+    print("Time to train", time.time() - t_start)
+
+test(warp.odefunc, timestamps_base, points)
+plt.show()
+# eval_flow(warp.odefunc)
