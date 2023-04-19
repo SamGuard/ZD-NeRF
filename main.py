3f1a94bde089a166ba2cd8a0313cc8b50d36abcb
Sam Guard <sg2268@bath.ac.uk>
2023-04-19 13:39:33 +0100

    changed rendering to use train dataset for a quick test

--- main.py
+++ main.py
@@ -393,9 +393,9 @@ if __name__ == "__main__":
                 for i in [
                     0
                 ]:  # range(len(test_dataset)): #[int(t * num_time) % len(test_dataset)]:#
-                    data = train_dataset[i]
+                    data = test_dataset[i]
                     render_bkgd = data["color_bkgd"]
-                    rays = data["rays"]
+                    rays = train_dataset[0]["rays"]# data["rays"]
                     pixels = data["pixels"]
                     timestamps[0][0] = t
 
