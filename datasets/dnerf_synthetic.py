cabeee82e689763e306bc81e57f8cbb009a10fdb
Sam Guard <sg2268@bath.ac.uk>
2023-04-26 07:49:44 +0100

    printing

--- datasets/dnerf_synthetic.py
+++ datasets/dnerf_synthetic.py
@@ -217,7 +217,6 @@ class SubjectLoader(torch.utils.data.Dataset):
             y = torch.randint(
                 0, self.HEIGHT, size=(num_rays,), device=self.images.device
             )
-            print(x, y, num_rays)
         else:
             image_id = [index]
             x, y = torch.meshgrid(
